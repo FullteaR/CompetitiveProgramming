1b7910a3ba9bf1bf15a8a90a479a4fb11ef9fb5e
Rikuta Furuta <rikuta@furutan.com>
2020-01-23 20:54:51 +0900

    DPL

---
+++ AOJ/DPL/Balls and Boxes 4.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+import sys
+mod=1000000007
+
+n,k=[int(i) for i in input().split(" ")]
+
+facts=[1 for i in range(n+k+1)]
+
+for i in range(1,n+k+1):
+    facts[i]=i*facts[i-1]
+
+@lru_cache()
+def combination(i,j):
+    return facts[i]//(facts[j]*facts[i-j])
+
+
+print(combination(n+k-1,n)%mod)
