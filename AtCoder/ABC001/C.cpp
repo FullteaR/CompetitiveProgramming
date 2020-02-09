9ef8645ae7ece43d1b15bfc1898defb9250ebecc
Rikuta Furuta <rikuta@furutan.com>
2020-02-09 19:29:00 +0900

    ABC001

--- AtCoder/ABC001/C.cpp
+++ AtCoder/ABC001/C.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main(void){
-        double Deg,Dis;
+        long double Deg,Dis;
         cin>>Deg>>Dis;
 
         int W=0;
         Dis=Dis/60.0;
+        if(100*Dis-((int)(10*Dis))*10>=5){
+          Dis+=0.1;
+        }
+        Dis=((double)(int)(10*Dis))/10.0;
         if (Dis<=0.2) {
                 W=0;
         }
@@ -56,46 +60,46 @@ int main(void){
                 if (112.5<=Deg && Deg<337.5) {
                         cout << "NNE";
                 }
-                else if(Deg<562.5) {
+                else if(Deg>=337.5 && Deg<562.5) {
                         cout << "NE";
                 }
-                else if(Deg<787.5) {
+                else if(Deg>=562.5 && Deg<787.5) {
                         cout << "ENE";
                 }
-                else if(Deg<1012.5) {
+                else if(Deg>=787.5 && Deg<1012.5) {
                         cout << "E";
                 }
-                else if(Deg<1237.5) {
+                else if(Deg>=1012.5 && Deg<1237.5) {
                         cout << "ESE";
                 }
-                else if(Deg<1462.5) {
+                else if(Deg>=1237.5 && Deg<1462.5) {
                         cout << "SE";
                 }
-                else if(Deg<1687.5) {
+                else if(Deg>=1462.5 && Deg<1687.5) {
                         cout << "SSE";
                 }
-                else if(Deg<1912.5) {
+                else if(Deg>=1687.5 && Deg<1912.5) {
                         cout << "S";
                 }
-                else if(Deg<2137.5) {
+                else if(Deg>=1912.5 && Deg<2137.5) {
                         cout << "SSW";
                 }
-                else if(Deg<2362.5) {
+                else if(Deg>=2137.5 && Deg<2362.5) {
                         cout << "SW";
                 }
-                else if(Deg<2587.5) {
+                else if(Deg>=2362.5 && Deg<2587.5) {
                         cout << "WSW";
                 }
-                else if(Deg<2812.5) {
+                else if(Deg>=2587.5 && Deg<2812.5) {
                         cout << "W";
                 }
-                else if(Deg<3037.5) {
+                else if(Deg>=2812.5 && Deg<3037.5) {
                         cout << "WNW";
                 }
-                else if(Deg<3262.5) {
+                else if(Deg>=3037.5 && Deg<3262.5) {
                         cout << "NW";
                 }
-                else if(Deg<3487.5) {
+                else if(Deg>=3262.5 && Deg<3487.5) {
                         cout << "NNW";
                 }
                 else{
