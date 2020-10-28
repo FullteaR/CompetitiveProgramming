eb9a4b8078ad24ad292b66eef406f125a242b107
Rikuta Furuta <rikuta@furutan.com>
2020-10-28 23:32:53 +0900

    Educational DP Contest

--- AtCoder/Educational DP Contest/E.cpp
+++ AtCoder/Educational DP Contest/E.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 using namespace std;
+#define INF 10000000010
 
 int main(void){
   long N,W;
   cin>>N>>W;
+
   long* dp[N+1];
   for(int i=0;i<=N;i++){
-    dp[i]=new long[W+1];
-  }
-
-  for(int i=0;i<=N;i++){
-    for(int j=0;j<=W;j++){
-      dp[i][j]=0;
+    dp[i] = new long[100010];
+    for(int j=0;j<100010;j++){
+      dp[i][j]=INF;
     }
+    dp[i][0]=0;
   }
   for(int i=1;i<=N;i++){
-    long w,v;
+    long v,w;
     cin>>w>>v;
-    for(int j=0;j<=W;j++){
-      dp[i][j]=dp[i-1][j];
-      if(j-w>=0){
-        dp[i][j]=max(dp[i][j],dp[i-1][j-w]+v);
+    for(int j=0;j<100010;j++){
+      if(j-v>=0){
+        dp[i][j]=min(dp[i-1][j],dp[i-1][j-v]+w);
+      }
+      else{
+        dp[i][j]=dp[i-1][j];
       }
     }
   }
-  long retval=0;
-  for(long i=0;i<=W;i++){
-    retval=max(retval,dp[N][i]);
+  for(long v=100010-1;v>=0;v--){
+    if(dp[N][v]<=W){
+      cout<<v<<endl;
+      break;
+    }
   }
-  cout<<retval<<endl;
+
   return 0;
 }
--- AtCoder/Educational DP Contest/F.cpp
+++ AtCoder/Educational DP Contest/F.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 int main(void){
   string* dp[3010];
+  long* len[3010];
   for(int i=0;i<3010;i++){
     dp[i] = new string[3010];
+    len[i] = new long[3010];
   }
   for(int i=0;i<3010;i++){
     dp[i][0]="";
+    len[i][0]=0;
   }
   for(int j=0;j<3010;j++){
     dp[0][j]="";
+    len[0][j]=0;
   }
   string s;
   string t;
@@ -22,20 +26,24 @@ int main(void){
   int t_size=t.size();
   for(int i=1;i<=s_size;i++){
     for(int j=1;j<=t_size;j++){
-      if(dp[i-1][j].size()>dp[i][j-1].size()){
+      if(len[i-1][j]>len[i][j-1]){
         dp[i][j]=dp[i-1][j];
+        len[i][j]=len[i-1][j];
       }
       else{
         dp[i][j]=dp[i][j-1];
+        len[i][j]=len[i][j-1];
       }
       if(s[i-1]==t[j-1]){
-        if(dp[i-1][j-1].size()+1>dp[i][j].size()){
+        if(len[i-1][j-1]+1>len[i][j]){
           dp[i][j]=dp[i-1][j-1]+s[i-1];
+          len[i][j]=len[i-1][j-1]+1;
         }
       }
       else{
-        if(dp[i-1][j-1].size()>dp[i][j].size()){
+        if(len[i-1][j-1]>len[i][j]){
           dp[i][j]=dp[i-1][j-1];
+          len[i][j]=len[i-1][j-1];
         }
       }
     }
