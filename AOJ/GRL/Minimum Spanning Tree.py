c5c3248c80811320a582c039a4e25265e6d60093
Rikuta Furuta <rikuta@furutan.com>
2020-12-31 21:00:35 +0900

    GRL

---
+++ AOJ/GRL/Minimum Spanning Tree.py
@@ -0,0 +1,64 @@
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
+
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+
+V,E=[int(i) for i in input().split(" ")]
+edges=[]
+for i in range(E):
+    edges.append([int(j) for j in input().split(" ")])
+
+uf=UnionFind(V)
+edges=sorted(edges,key=lambda w:w[2])
+total=0
+for e in edges:
+    if uf.same(e[0],e[1]):
+        continue
+    else:
+        uf.union(e[0],e[1])
+        total+=e[2]
+
+print(total)
